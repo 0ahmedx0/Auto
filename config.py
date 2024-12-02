c71ba6f21b3b8c53c36df80eb26390ff2caa2535
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2024-12-02 07:07:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "21417190")
+    API_HASH = environ.get("API_HASH", "49cd487bc068aa8ec3cd060a587017b0")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7518380330:AAH2Xub5gKYf4jYGT0-cJxdqotPpBL1rLLA") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
+    DATABASE_URI = environ.get("DATABASE", "cluster0-shard-00-01.quu8v.mongodb.net:27017")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8077445518').split()]
 
 class temp(object): 
     lock = {}
@@ -15,4 +15,6 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
+    # safe_repo
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
