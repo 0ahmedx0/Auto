7ef5f93531748d75521314ef58742d4a978a5026
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2024-12-02 08:53:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "21417190")
     API_HASH = environ.get("API_HASH", "49cd487bc068aa8ec3cd060a587017b0")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7518380330:AAH2Xub5gKYf4jYGT0-cJxdqotPpBL1rLLA") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8158734648:AAEKvePMHcNdBIHCbiCVIP-CQZtr40VFcB4") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "cluster0-shard-00-01.quu8v.mongodb.net:27017")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
